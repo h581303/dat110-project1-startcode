b0277445220e630130016de326b25cde2b750cff
TheSaltChip <seb2500@gmail.com>
2021-01-29 15:33:07 +0100

    Implemented a check for length of the given payload, JavaDoc-ed and refactored

--- dat110-project1/src/no/hvl/dat110/messaging/Message.java
+++ dat110-project1/src/no/hvl/dat110/messaging/Message.java
@@ -1,57 +1,65 @@
 package no.hvl.dat110.messaging;
 
-import java.util.Arrays;
-
-import no.hvl.dat110.TODO;
-
 public class Message {
 
-	private byte[] payload;
+    private byte[] payload;
 
-	public Message(byte[] payload) {
-		this.payload = payload; // TODO: check for length within boundary
-		// payload.length() <= 127
-	}
+    /**
+     * Creates a new Message object with the given payload as payload
+     *
+     * @param payload The information that is going to be sent, which must be less than 128 bytes
+     * @throws Exception If the payload is over 127 bytes this get thrown
+     */
+    public Message(byte[] payload) throws Exception {
+        if (payload.length > 127) {
+            throw new Exception("Payload to big");
+        }
 
-	public Message() {
-		super();
-	}
+        this.payload = payload; // TODO: check for length within boundary
+        // payload.length() <= 127
+    }
 
+    public Message() {
+        super();
+    }
+
+    /**
+     * Converts the payload into a 128 byte long array
+     *
+     * @return byte array ready to be sent
+     */
+    public byte[] encapsulate() {
+
+        byte[] encoded = new byte[128];
+
+        encoded[0] = (byte) payload.length;
+
+        System.arraycopy(payload, 0, encoded, 1, payload.length);
+
+        return encoded;
+    }
+
+    /**
+     * Converts the received byte array into a byte array without the padding and first byte
+     *
+     * @param received 128 byte long array containing information
+     */
+    public void decapsulate(byte[] received) {
+
+        byte[] buffer = new byte[received[0]];
+
+        if (buffer.length >= 0) System.arraycopy(received, 1, buffer, 0, buffer.length);
+
+        payload = buffer;
+    }
+
+	/**
+	 * Returns the data contained in this message
+	 *
+	 * @return The payload
+	 */
 	public byte[] getData() {
-		return this.payload; 
+		return this.payload;
 	}
 
-	public byte[] encapsulate() {
-		
-		byte[] encoded = new byte[128];
-		
-		encoded[0] = (byte)payload.length;
-		
-		int i = 1;
-		
-		while(i <= payload.length) {
-			encoded[i] = payload[i-1];
-			i++;
-		}
-
-		return encoded;
-		
-	}
-
-	public void decapsulate(byte[] received) {
-		
-		byte[] buffer = new byte[received[0]];
-		
-		int i = 0;
-		
-		while(i < buffer.length) {
-			buffer[i] = received[i+1];
-			i++;
-		}
-		
-		
-		
-		payload = buffer;
-		
-	}
 }
