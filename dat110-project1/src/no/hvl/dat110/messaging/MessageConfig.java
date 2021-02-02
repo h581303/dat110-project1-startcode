159545b0e1377427c6b21987844ade775da37628
TheSaltChip <seb2500@gmail.com>
2021-02-02 22:57:29 +0100

    changed to port 9090, since 8080 is used by something, might be the postgres db on my desktop

--- dat110-project1/src/no/hvl/dat110/messaging/MessageConfig.java
+++ dat110-project1/src/no/hvl/dat110/messaging/MessageConfig.java
@@ -4,6 +4,6 @@ public class MessageConfig {
 
 	public static final int SEGMENTSIZE = 128;
 	
-	public static final int MESSAGINGPORT = 8080;
+	public static final int MESSAGINGPORT = 9090;
 	public static final String MESSAGINGHOST = "localhost";
 }
