3586b2645fb9bffc1aae7117c9e045b3c04e5833
TheSaltChip <seb2500@gmail.com>
2021-02-04 18:28:19 +0100

    Completed the TODOs and formatted

--- dat110-project1/src/no/hvl/dat110/system/controller/Controller.java
+++ dat110-project1/src/no/hvl/dat110/system/controller/Controller.java
@@ -1,55 +1,64 @@
 package no.hvl.dat110.system.controller;
 
-import no.hvl.dat110.TODO;
 import no.hvl.dat110.rpc.RPCClient;
 import no.hvl.dat110.rpc.RPCServerStopStub;
 
-public class Controller  {
-	
-	private static int N = 5;
-	
-	public static void main (String[] args) {
-		
-		Display display;
-		Sensor sensor;
-		
-		RPCClient displayclient,sensorclient;
-		
-		System.out.println("Controller starting ...");
-				
-		RPCServerStopStub stopdisplay = new RPCServerStopStub();
-		RPCServerStopStub stopsensor = new RPCServerStopStub();
-		
-		displayclient = new RPCClient(Common.DISPLAYHOST,Common.DISPLAYPORT);
-		sensorclient = new RPCClient(Common.SENSORHOST,Common.SENSORPORT);
-		
-		// TODO
-		// connect to sensor and display RPC servers
-		// create local display and sensor objects
-		// register display and sensor objects in the RPC layer
-		
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-		}
-		
-		// register stop methods in the RPC layer
-		displayclient.register(stopdisplay);
-		sensorclient.register(stopsensor);
-		
-		// TODO:
-		// loop while reading from sensor and write to display via RPC
-		
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
+public class Controller {
+
+    private static int N = 5;
+
+    public static void main(String[] args) {
+
+        Display display;
+        Sensor sensor;
+
+        RPCClient displayClient, sensorClient;
+
+        System.out.println("Controller starting ...");
+
+        RPCServerStopStub stopDisplay = new RPCServerStopStub();
+        RPCServerStopStub stopSensor = new RPCServerStopStub();
+
+        displayClient = new RPCClient(Common.DISPLAY_HOST, Common.DISPLAY_PORT);
+        sensorClient = new RPCClient(Common.SENSOR_HOST, Common.SENSOR_PORT);
+
+        // TODO
+        // connect to sensor and display RPC servers
+        // create local display and sensor objects
+        // register display and sensor objects in the RPC layer
+
+        displayClient.connect();
+        sensorClient.connect();
+
+        display = new Display();
+        sensor = new Sensor();
+
+        sensorClient.register(sensor);
+        displayClient.register(display);
+
+        // register stop methods in the RPC layer
+        displayClient.register(stopDisplay);
+        sensorClient.register(stopSensor);
+
+        // TODO:
+        // loop while reading from sensor and write to display via RPC
+
+		for (int i = 0; i < 5; i++) {
+			display.write(sensor.read() + "");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		
-		stopdisplay.stop();
-		stopsensor.stop();
-	
-		displayclient.disconnect();
-		sensorclient.disconnect();
-		
-		System.out.println("Controller stopping ...");
-		
-	}
+		}
+
+        stopDisplay.stop();
+        stopSensor.stop();
+
+        displayClient.disconnect();
+        sensorClient.disconnect();
+
+        System.out.println("Controller stopping ...");
+
+    }
 }
